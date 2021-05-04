6afca16f2fb7b195ca3b04ac711c414593386537
Vishwas Singh <singh.vishu234@gmail.com>
2021-05-04 22:23:01 +0530

    [Fix]:comment out unused API code

--- app.js
+++ app.js
@@ -37,77 +37,79 @@ mongoose.connect(
 const port = process.env.PORT || 3000;
 app.listen(port);
 
+//to be fired only when using post route:
+
 //sheets api
-const { google } = require("googleapis");
-const keys = require('./keys.json')
+// const { google } = require("googleapis");
+// const keys = require('./keys.json')
 
-const client = new google.auth.JWT(
-    keys.client_email, 
-    null, 
-    keys.private_key,
-    ['https://www.googleapis.com/auth/spreadsheets'] 
-);
+// const client = new google.auth.JWT(
+//     keys.client_email, 
+//     null, 
+//     keys.private_key,
+//     ['https://www.googleapis.com/auth/spreadsheets'] 
+// );
 
-client.authorize(function(err, tokens) {
-    if(err){
-        console.log(err);
-        return;
-    } else {
-        console.log('connected to google API');
-        gsrun(client);
-    }
-});
+// client.authorize(function(err, tokens) {
+//     if(err){
+//         console.log(err);
+//         return;
+//     } else {
+//         console.log('connected to google API');
+//         gsrun(client);
+//     }
+// });
 
-var s;
+// var s;
 
-async function gsrun(cl){
-    const gsapi = google.sheets({version:'v4', auth:cl});
+// async function gsrun(cl){
+//     const gsapi = google.sheets({version:'v4', auth:cl});
 
-    const opt = {
-        spreadsheetId: '1Y0b8HYrWcHNLIo08y3lp4KU-ePt2X-Vh7l6J2ex1bYo',
-        range:'table'
-    }
+//     const opt = {
+//         spreadsheetId: '1Y0b8HYrWcHNLIo08y3lp4KU-ePt2X-Vh7l6J2ex1bYo',
+//         range:'table'
+//     }
 
-    let data1 = await gsapi.spreadsheets.values.get(opt);
+//     let data1 = await gsapi.spreadsheets.values.get(opt);
 
-    const act_data = data1.data.values;
+//     const act_data = data1.data.values;
 
-    s = act_data;
-    console.log(s[5][3]);
-}
+//     s = act_data;
+//     console.log(s[5][3]);
+// }
 
-app.post('/posts1', async (req, res) => {
+// app.post('/posts1', async (req, res) => {
 
-    for(var i=2;i<s.length;i++)
-    {
+//     for(var i=2;i<s.length;i++)
+//     {
 
-        if(s[i][0]=='')
-        {
-            for(var j=0;j<=9;j++)
-            {
-                s[i][j] = (s[i][j] == "") ? s[i-1][j] : s[i][j];
-            }
-        }
+//         if(s[i][0]=='')
+//         {
+//             for(var j=0;j<=9;j++)
+//             {
+//                 s[i][j] = (s[i][j] == "") ? s[i-1][j] : s[i][j];
+//             }
+//         }
 
-        const post = new Post({
-            Code: s[i][0],
-            Sex: s[i][1],
-            Age: s[i][2],
-            BodyWeight: s[i][3],
-            BrainWeight: s[i][4],
-            Parts: s[i][5],
-            Staining: s[i][6],
-            SectionThickness: s[i][7],
-            PlaneOfSectioning: s[i][8],
-            Distance: s[i][9],
-        });
-        try{
-            const savedPost = await post.save();
-            // res.json(savedPost);    
-        }catch(err) {
-            res.json({message: err})
-        }
+//         const post = new Post({
+//             Code: s[i][0],
+//             Sex: s[i][1],
+//             Age: s[i][2],
+//             BodyWeight: s[i][3],
+//             BrainWeight: s[i][4],
+//             Parts: s[i][5],
+//             Staining: s[i][6],
+//             SectionThickness: s[i][7],
+//             PlaneOfSectioning: s[i][8],
+//             Distance: s[i][9],
+//         });
+//         try{
+//             const savedPost = await post.save();
+//             // res.json(savedPost);    
+//         }catch(err) {
+//             res.json({message: err})
+//         }
 
-    }
-    res.redirect('/posts');
-})
+//     }
+//     res.redirect('/posts');
+// })
